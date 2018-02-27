218c4a064e139e4d2c4faa7c26d95b959784b1e7
Zartei <Zirdii@gmail.com>
2018-02-27 13:39:41 +0100

    numersTolookFor

--- uppgift2/myjsfile.js
+++ uppgift2/myjsfile.js
@@ -1,17 +1,38 @@
-
-var sok = 8;
+var sok = Math.floor((Math.random()*20));
+var attempts = 0;
 var output = "Not set"
+
 function numberSeek(){
     var tal = document.getElementById("input").value;
+    attempts++;
     if (tal > sok) {
         output = "För högt";
+        
     } 
     else if (tal < sok) {
         output = "För lågt";
+        
     } 
     else {
-        output = "Rätt Svar";
+        output = "Rätt Svar på "+ attempts +" försök";
+        attempts = 0;
     }
-console.log(output);
-document.getElementById("output").innerHTML = output;
+    console.log(output);
+    document.getElementById("output").innerHTML = output;
+}
+
+function numberToLookFor(){
+    for (var i = 0; i < arguments.length;i++){
+        console.log("Trying: " + arguments[i]);
+        if(arguments[i] > sok) {
+            output = "För högt";
+        } 
+        else if (arguments[i] < sok) {
+            output = "För lågt";    
+        } 
+        else {
+            output = "Rätt Svar";
+        }
+        console.log(arguments[i] + " är "+ output);
+    }
 }
